9c8a7bd02c952c38b280fe1c4f4e3834826ec2da
ThinhPhoenix <laithinh1310@gmail.com>
2025-12-04 23:25:27 +0700

    feat: add payout table and update timestamp default values in payment schema

--- services/payment-service/db.sql
+++ services/payment-service/db.sql
@@ -1,7 +1,11 @@
 DROP DATABASE IF EXISTS payment_service;
 CREATE DATABASE payment_service;
 
+-- Connect to the database first
+\c payment_service
+
 CREATE TYPE payment_status AS ENUM ('pending', 'completed', 'failed', 'refunded', 'cancelled', 'expired');
+CREATE TYPE payout_status AS ENUM ('pending', 'completed');
 
 CREATE TABLE payments (
     id VARCHAR PRIMARY KEY,
@@ -12,8 +16,8 @@ CREATE TABLE payments (
     checkout_url VARCHAR(255),
     type VARCHAR(50),
     order_code VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh',
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh',
+    created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh'),
+    updated_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh'),
     expired_at TIMESTAMP,
     paid_at TIMESTAMP,
     deleted_at TIMESTAMP
@@ -26,8 +30,22 @@ CREATE TABLE order_items (
     name VARCHAR NOT NULL,
     price DECIMAL(12,2) NOT NULL,
     quantity INT NOT NULL DEFAULT 1,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh',
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh',
+    created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh'),
+    updated_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh'),
     deleted_at TIMESTAMP,
     FOREIGN KEY (payment_id) REFERENCES payments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE payouts (
+    id VARCHAR PRIMARY KEY,
+    amount DECIMAL(12,2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    status payout_status NOT NULL DEFAULT 'pending',
+    user_id VARCHAR NOT NULL,
+    bank_code VARCHAR(255),
+    account_number VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh'),
+    updated_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Ho_Chi_Minh'),
+    deleted_at TIMESTAMP,
+    completed_at TIMESTAMP
 );
